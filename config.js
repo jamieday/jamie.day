ad244349995bfa764cc6dbfa96563845c9e58968
Jamie <jamie@warrantymaster.com>
2017-08-06 12:35:29 -0400

    empty config.js

--- config.js
+++ config.js
@@ -1,8 +1,2 @@
-var config = {
-  linkedIn: {
-    clientId: "77da3xwfbgtm0t",
-    clientSecret: "fG64OuqR0vgNF4DU",
-    callbackUrl: "https://jamieday.herokuapp.com/auth/linkedin/callback"
-  }
-};
+var config = {};
 module.exports = config;
