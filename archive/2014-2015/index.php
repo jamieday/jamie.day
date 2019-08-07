d9ec7bc9328da1ad076d2a885c05962ab1fa7111
Jamie Day <jamie@jamieday.ca>
2019-08-07 19:03:50 -0400

    add old archive

---
+++ archive/2014-2015/index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<!--
+This is copywrited and all that
+-->
+<html>
+
+  <head>
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <script src="code.js"></script>
+    <title>Jamie Day</title>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-53691427-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+  </head>
+
+  <body>
+    <div class="nav">
+      <div class="container">
+        <p>
+        <ul class="pull-left">
+          <li><a href="#">Home</a></li>
+          <li><a href="numbers.html">About Us</a></li>
+		  <li><a href="games.html">Games</li>
+        </ul>
+        <ul class="pull-right">
+          <li><a href="numbers.html">Sign Up</a></li>
+          <li><a href="numbers.html">Log In</a></li>
+          <li><a href="numbers.html">Help</a></li>
+        </ul>
+        </p>
+      </div>
+    </div>
+
+    <div class="jumbotron">
+      <div class="container">
+        <h1>This website is under development.</h1>
+        <p>So there's really nothing to do here.</p>
+        <a href="numbers.html">Learn More</a>
+      </div>
+    </div> 
+    <div class="about-me">
+      <div class="container">
+      <h2>Random Pictures</h2>
+      <p>Here are those random pictures you asked for.</p>
+      <div class="row">
+        <div class="col-md-4">
+          <div class="thumbnail">
+            <img src="http://upload.wikimedia.org/wikipedia/commons/b/b0/Sierra_Nevada_Albert_Bierstadt_circa_1871.jpeg">
+          </div>
+          <div class="thumbnail">
+            <img src="http://fc05.deviantart.net/fs70/i/2011/299/d/8/wallpaper_creativity_by_backfirejr-d4e0iyg.png">
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="thumbnail">
+            <img src="http://fc02.deviantart.net/fs71/i/2013/064/b/8/background_space_by_billyfabiancow-d5x321d.png">
+          </div>
+          <div class="thumbnail">
+            <img src="https://c2.staticflickr.com/6/5216/5518991291_8c8164c5cf.jpg">
+          </div>
+        </div>
+      </div>
+      </div>
+    </div>
+    <div class="learn-more">
+	  <div class="container">
+		<div class="row">
+	      <div class="col-md-4">
+			<h3>Software Development</h3>
+			<p>In development.</p>
+			<p><a href="#">Learn more about my software engineering pursuits.</a></p>
+	      </div>
+		  <div class="col-md-4">
+			<h3>Web Development & Design</h3>
+			<p>Discover the power of web design and development. Watch as we strive to create a stronger, simpler, and more beautiful web.</p>
+			<p><a href="http://upload.wikimedia.org/wikipedia/commons/0/08/Araneus_diadematus_web_1.jpg">Learn more about my web design and development.</a></p>
+		  </div>
+		  <div class="col-md-4">
+			<h3>Number Development</h3>
+			<p>See unique numbers created by Jamie Day. <p>For example, &image;.</p></p>
+			<p><a href="numbers.html">Learn about number development.</a></p>
+		  </div>
+	    </div>
+	  </div>
+	</div>
+  </body>
+</html>
