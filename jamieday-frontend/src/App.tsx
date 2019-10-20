8fe88a0086bfe58793f6c5e76a3a30e9eb0aedc9
Jamie Day <jamie@jamieday.ca>
2019-10-20 16:11:11 -0400

    wip add loginsecure to website

--- jamieday-frontend/src/App.tsx
+++ jamieday-frontend/src/App.tsx
@@ -1,30 +1,140 @@
-import React from "react";
-import logo from "./cone-icon.png";
-import "./App.css";
+import Autosuggest from 'react-autosuggest';
+import React from 'react';
+import './App.css';
+import { ChangeEvent } from 'react';
+
+const inputStyles = {
+  backgroundColor: '#ddd',
+  border: '1px solid gray',
+  color: '#454545',
+  marginBottom: 25,
+  padding: 15,
+  borderRadius: 5
+};
+
+const Input: React.FunctionComponent<any> = ({
+  placeholder,
+  ...props
+}) => (
+  <input
+    style={inputStyles}
+    type="text"
+    placeholder={placeholder}
+    {...props}
+  />
+);
+
+type Suggestion = string;
+const commonPasswords: Suggestion[] = [
+  'password',
+  'p4ssword',
+  'ashley'
+];
+const renderSuggestion = (suggestion: Suggestion) => (
+  <div style={{ background: 'black' }}>{suggestion}</div>
+);
+const PasswordAuto: React.FunctionComponent = ({}) => {
+  const [password, setPassword] = React.useState('');
+  const [suggestions, setSuggestions] = React.useState<
+    Suggestion[]
+  >([]);
+
+  return (
+    <Autosuggest
+      // TODO common password list
+      suggestions={suggestions}
+      onSuggestionsFetchRequested={() =>
+        setSuggestions(commonPasswords)
+      }
+      onSuggestionsClearRequested={() => setSuggestions([])}
+      getSuggestionValue={x => x}
+      renderSuggestion={renderSuggestion}
+      inputProps={{
+        type: 'password',
+        placeholder: 'Enter your password...',
+        value: password,
+        onChange: (e, { newValue }) => setPassword(newValue)
+      }}
+      renderInputComponent={props => <Input {...props} />}
+    ></Autosuggest>
+  );
+};
+
+const LoginSecureLogo: React.FunctionComponent = () => (
+  <div className="Title">
+    <span>log</span>
+    <span style={{ textDecoration: 'underline' }}>in</span>
+    <span
+      style={{
+        color: '#0d0',
+        marginBottom: 15
+      }}
+    >
+      secure
+    </span>
+  </div>
+);
+
+const LoginSecure: React.FunctionComponent = () => {
+  const [email, setEmail] = React.useState('');
+
+  return (
+    <div
+      style={{
+        border: '1px solid rgba(88,88,88,.66)',
+        background: '#272e3a',
+        borderRadius: 10,
+        width: 500
+      }}
+    >
+      <header className="App-header">
+        <LoginSecureLogo />
+
+        <Input
+          placeholder="Enter your email..."
+          value={email}
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            setEmail(e.target.value)
+          }
+        />
+        <PasswordAuto />
+        <input
+          style={{ ...inputStyles, ...{ margin: '15px 0 0 0' } }}
+          type="submit"
+          value="Sign in"
+        />
+      </header>
+    </div>
+  );
+};
 
 const App: React.FunctionComponent = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p style={{ fontSize: 28, marginTop: 0 }}>
-          Recently decided to redo this site from scratch.
-        </p>
-        <p style={{ fontSize: 20, marginTop: 5 }}>
-          Won't take me too long to get something up and running.
-        </p>
+      <div style={{ margin: 15 }}>
+        <LoginSecure />
+      </div>
+      <div style={{ color: 'white' }}>
         <p style={{ fontSize: 16, marginTop: 8 }}>
-          In the meantime, check out my{" "}
-          <a className="App-link" href="https://www.linkedin.com/in/dayjamie/">
+          This is a side-project I'm working on.
+          <br />
+          <br /> Check out my{' '}
+          <a
+            className="App-link"
+            href="https://www.linkedin.com/in/dayjamie/"
+          >
             LinkedIn
-          </a>{" "}
-          and{" "}
-          <a className="App-link" href="https://github.com/jamieday">
+          </a>{' '}
+          and{' '}
+          <a
+            className="App-link"
+            href="https://github.com/jamieday"
+          >
             GitHub
-          </a>
-          .
+          </a>{' '}
+          for more.
         </p>
-      </header>
+      </div>
     </div>
   );
 };
