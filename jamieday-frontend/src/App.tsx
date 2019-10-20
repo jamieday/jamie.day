7886b9ee074be5565d72fc9736b5d66557c11193
Jamie Day <jamie@jamieday.ca>
2019-10-20 16:11:11 -0400

    wip add loginsecure to website

--- jamieday-frontend/src/App.tsx
+++ jamieday-frontend/src/App.tsx
@@ -1,30 +1,66 @@
-import React from "react";
-import logo from "./cone-icon.png";
-import "./App.css";
+import React from 'react';
+import './App.css';
+
+type InputProps = { placeholder: string };
+const Input: React.FunctionComponent<InputProps> = ({
+  placeholder
+}) => (
+  <input
+    style={{
+      backgroundColor: '#ddd',
+      border: '1px solid gray',
+      color: 'gray',
+      margin: 15,
+      padding: 15,
+      borderRadius: 5
+    }}
+    type="text"
+    placeholder={placeholder}
+  />
+);
 
 const App: React.FunctionComponent = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p style={{ fontSize: 28, marginTop: 0 }}>
-          Recently decided to redo this site from scratch.
-        </p>
-        <p style={{ fontSize: 20, marginTop: 5 }}>
-          Won't take me too long to get something up and running.
-        </p>
-        <p style={{ fontSize: 16, marginTop: 8 }}>
-          In the meantime, check out my{" "}
-          <a className="App-link" href="https://www.linkedin.com/in/dayjamie/">
-            LinkedIn
-          </a>{" "}
-          and{" "}
-          <a className="App-link" href="https://github.com/jamieday">
-            GitHub
-          </a>
-          .
-        </p>
-      </header>
+      <div>
+        <header className="App-header">
+          <div className="Title">
+            <span>log</span>
+            <span style={{ textDecoration: 'underline' }}>in</span>
+            <span
+              style={{
+                color: '#0d0'
+              }}
+            >
+              secure
+            </span>
+          </div>
+
+          <Input placeholder="Enter your email..." />
+          <Input placeholder="Enter your password..." />
+          <input
+            style={{ padding: '5px 15px', backgroundColor: '#ddd' }}
+            type="submit"
+            value="Sign in"
+          />
+        </header>
+      </div>
+      <p style={{ fontSize: 16, marginTop: 8 }}>
+        This is a side-project I'm working on called LoginSecure.
+        <br />
+        <br /> Check out my{' '}
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/dayjamie/"
+        >
+          LinkedIn
+        </a>{' '}
+        and{' '}
+        <a className="App-link" href="https://github.com/jamieday">
+          GitHub
+        </a>{' '}
+        for more.
+      </p>
     </div>
   );
 };
